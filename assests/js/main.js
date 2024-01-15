ba8f3309874e16a2751962d3bba0d6771501364f
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-01-15 11:59:56 +0530

    Added contact JS part

--- assests/js/main.js
+++ assests/js/main.js
@@ -100,14 +100,29 @@ modelCloses.forEach((modelClose) => {
 
 
 
+/*==========INPUT ANIMATION=================*/
+/*
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+})
+*/
 
 
-
-
-
-
-
-
+/*==========Scroll Section Active Link=================*/
 
 
 
